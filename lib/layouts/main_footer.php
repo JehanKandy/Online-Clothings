ce76f8fd14fda565193f723a7b4e5f17297c990b
JehanKandy <jehankandy@gmail.com>
2023-01-19 20:15:30 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -16,8 +16,24 @@
                     <div class="fitem2">
                         <div class="title">Usefull Links</div>
                     </div>
+                    <div class="body">
+                        <ul>
+                            <li><a href="#">Buy Online</a></li>
+                            <li><a href="#">Buy From Shop</a></li>
+                            <li><a href="#">Get Discount</a></li>
+                            <li><a href="#">Link 1</a></li>                            
+                        </ul>
+                    </div>
                     <div class="fitem3">
                         <div class="title">Our Services</div>
+                        <div class="body">
+                            <ul>
+                                <li><a href="#">Online Store</a></li>
+                                <li><a href="#">Shops</a></li>
+                                <li><a href="#">Island wide Delivery</a></li>
+                                <li><a href="#">Service 1</a></li>                            
+                            </ul>
+                        </div>
                     </div>
                     <div class="fitem4">
                         <div class="title">Contact Us</div>
