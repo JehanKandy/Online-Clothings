3f51ea99e30783f067e23185ae3c91872bc296b1
JehanKandy <jehankandy@gmail.com>
2023-01-19 20:04:05 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -1,5 +1,18 @@
 <link rel="stylesheet" href="../../css/style.css">
 
-
+<footer>
+    <div class="main-footer">
+        <div class="container">
+            <div class="body">
+                <div class="footer-grid">
+                    <div class="fitem1">Item 1</div>
+                    <div class="fitem2">Item 2</div>
+                    <div class="fitem3">Item 3</div>
+                    <div class="fitem4">Item 4</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
 <script src="../../js/script.js"></script>
