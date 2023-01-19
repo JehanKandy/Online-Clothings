17b2564bea546ec88135f53b8a8ab3ea9f946bb7
JehanKandy <jehankandy@gmail.com>
2023-01-19 20:10:23 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -4,8 +4,15 @@
     <div class="main-footer">
         <div class="container">
             <div class="body">
+                <div class="footer-title">Hiruni Clothings</div>
+                <hr class="footer-hr">
                 <div class="footer-grid">
-                    <div class="fitem1">Item 1</div>
+                    <div class="fitem1">
+                        <div class="title">Hiruni Clothings</div>
+                    </div>
+                    <div class="body">
+                        The Online Clothings Develop for educational purposes only
+                    </div>
                     <div class="fitem2">Item 2</div>
                     <div class="fitem3">Item 3</div>
                     <div class="fitem4">Item 4</div>
