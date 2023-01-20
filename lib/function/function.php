4c4dee676c1caed46c498da02306a82093bf2d63
JehanKandy <jehankandy@gmail.com>
2023-01-20 19:21:38 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -7,7 +7,12 @@
 
     function subsctibe($email){
         $con = Connection();
+
+        $select_data = "SELECT * FROM subscribe_tbl WHERE email = '$email'";
+        $select_data_result = mysqli_query($con, $select_data);
         
+
+
     }
 
 
