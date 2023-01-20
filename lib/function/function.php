a73619d303acc3d4f58f5e1cac7098dd7b18722d
JehanKandy <jehankandy@gmail.com>
2023-01-20 19:22:42 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -10,7 +10,9 @@
 
         $select_data = "SELECT * FROM subscribe_tbl WHERE email = '$email'";
         $select_data_result = mysqli_query($con, $select_data);
-        
+        $select_data_nor = mysqli_num_rows($select_data_result);
+        $select_data_row = mysqli_fetch_assoc($select_data_result);
+
 
 
     }
