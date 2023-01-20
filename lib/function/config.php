696a158c2983e2370e3a7319e24be9414c250397
JehanKandy <jehankandy@gmail.com>
2023-01-20 19:09:36 +0530

    Update config.php

--- lib/function/config.php
+++ lib/function/config.php
@@ -6,6 +6,7 @@
         $db_name = "online_clothing_db";
 
         $con = mysqli_connect($server, $user, $pass, $db_name);
+        
         $result = (!$con)?"Connection Lost":$con;
         return $result;        
     }
