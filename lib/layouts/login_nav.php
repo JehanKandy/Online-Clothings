114270e6e95e79e27b75c82b23c6e816e80d3019
JehanKandy <jehankandy@gmail.com>
2023-01-20 19:36:41 +0530

    Add files via upload

---
+++ lib/layouts/login_nav.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="../../css/style.css">
+<nav>
+    <div class="nav-content">
+        <div class="container">
+            <div class="left-side">
+                Pearl Clothings
+            </div>
+            <div class="right-side">
+                <a href=""><button class="login-nav-btn">Login</button></a>
+            </div>                       
+        </div>
+    </div>
+</nav>
+
+<script src="../../js/script.js"></script>
