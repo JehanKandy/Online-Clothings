e8a9872b9e595560d0559d27ba8105f2883c4580
JehanKandy <jehankandy@gmail.com>
2023-01-20 19:28:27 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -24,7 +24,14 @@
                         </button>
                 </div>";
             }
-
+            else{
+                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Process Error</strong>Cannot Process the Request..!
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                        </button>
+                </div>";
+            }
         }
         else{
             return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
