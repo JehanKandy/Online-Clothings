b0283ec9d175b248c2e57fbf39653507b06d5fa4
JehanKandy <jehankandy@gmail.com>
2023-01-21 12:10:10 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -5,7 +5,23 @@
 
 <div class="reg-content">
     <div class="container">
+        <div class="body">
+            <div class="title"> <i class="fas fa-user-alt"></i> &nbsp; Login Here</div>
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
+                <div class="login-from">
+                    <input type="text" name="username" id="" required="required">
+                    <span>Username</span>
+                </div> 
+                <div class="login-from">
+                    <input type="password" name="pass" id="" required="required">
+                    <span>Password</span>
+                </div> 
 
+                <input type="submit" value="login" class="login-btn" name="login">
+            </form>
+            <a href="forget_pass.php" style="color: orange;">Forget Password</a>
+            <p style="color: white;">Don't have an Account ? <a href="reg.php" style="color: orange;">Sign Up</a></p>
+        </div>
     </div>
 </div>
 
