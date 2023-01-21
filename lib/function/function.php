d52d81465dfd1dbd979b114851a4fbd05ecc7618
JehanKandy <jehankandy@gmail.com>
2023-01-21 17:45:01 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -156,15 +156,8 @@
             $check_user_nor = mysqli_num_rows($check_user_result);
             $check_user_row = mysqli_fetch_assoc($check_user_result);
 
-            $check_user_deactive = "SELECT * FROM user_tbl WHERE is_pending = 1 && is_active = 0";
-            $check_user_deactive_result = mysqli_query($con, $check_user_deactive);
-            $check_user_deactive_nor = mysqli_num_rows($check_user_deactive_result);
-            $check_user_deactive_row = mysqli_fetch_assoc($check_user_deactive_result);
-
             if($check_user_nor != 0){
-                if($check_user_deactive_nor == 0){
 
-                }
             }else{
 
             }
