87a1b933d442fa55975adab3146d8cc6f080aa1f
JehanKandy <jehankandy@gmail.com>
2023-01-21 17:54:52 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -176,7 +176,12 @@
                         }
                     }
                     else{
-
+                        return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                                <strong>User Error</strong>User Doesn't Exists..!
+                                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                                <span aria-hidden='true'>&times;</span>
+                                </button>
+                        </div>";
                     }
                 }
             }else{
