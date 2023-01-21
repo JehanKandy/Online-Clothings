49568dadeceebdf641f3fcc407e4cce9a221104e
JehanKandy <jehankandy@gmail.com>
2023-01-21 12:25:11 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -77,6 +77,14 @@
                     </button>
             </div>";
         }
+        elseif($pass != $cpass){
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                    <strong>Password Error</strong>Password not Match..!
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+            </div>";
+        }
     }
 
 
