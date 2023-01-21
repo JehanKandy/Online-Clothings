e9785512441e59662d02484eb4e3b9c1613ae42b
JehanKandy <jehankandy@gmail.com>
2023-01-21 12:19:48 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -9,7 +9,8 @@
             <div class="title"> <i class="fas fa-user-alt"></i> &nbsp; Login Here</div>
             <?php 
                 if(isset($_POST['register'])){
-                    
+                    $result = reg_user($_POST['username'], $_POST['email'], md5($_POST['pass']), md5($_POST['cpass']));
+                    echo $result;
                 }
             ?>
 
