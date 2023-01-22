eff1ae6de7c3ddee4ad7fa51995bce192a402e9f
JehanKandy <jehankandy@gmail.com>
2023-01-22 21:11:23 +0530

    Update user.php

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -3,6 +3,12 @@
 <?php include("../layouts/header.php"); ?>
 
 
+<div class="product-view">
+    <div class="container">
+        Hi
+    </div>
+</div>
+
 
 <?php include("../layouts/page_footer.php"); ?>
 <script src="../../js/script.js"></script>
