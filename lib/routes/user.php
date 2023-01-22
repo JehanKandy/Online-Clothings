35b462d760693bfe1df8ccf126aadd39a0e5f614
JehanKandy <jehankandy@gmail.com>
2023-01-22 21:08:57 +0530

    Add files via upload

---
+++ lib/routes/user.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include("../function/function.php"); ?>
+<?php include("../layouts/header.php"); ?>
+
+
+
+<?php include("../layouts/page_footer.php"); ?>
+<script src="../../js/script.js"></script>
