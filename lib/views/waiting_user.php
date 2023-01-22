fd34080888491d2fc4364743d4f9ac8e8dc3eda7
JehanKandy <jehankandy@gmail.com>
2023-01-22 07:02:20 +0530

    Update waiting_user.php

--- lib/views/waiting_user.php
+++ lib/views/waiting_user.php
@@ -8,7 +8,8 @@
         <div class="body">
             <div class="title"> <i class="fas fa-user-clock"></i> &nbsp; Waiting User</div>
             <hr class="waiting-hr">
-            
+            <h4>Hi <?php waiting_user(); ?>...!</h4>
+            <p>Your Account Approval is Still Pending</p>
         </div>
     </div>
 </div>
