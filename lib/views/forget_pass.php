936e1e60bc9a78b3c8aef7802e9b0d8c29fafc4e
JehanKandy <jehankandy@gmail.com>
2023-01-22 07:26:18 +0530

    Update forget_pass.php

--- lib/views/forget_pass.php
+++ lib/views/forget_pass.php
@@ -6,7 +6,9 @@
 <div class="forget-pass-content">
     <div class="container">
         <div class="body">
-            <i class="fa fa-key"></i> Forget Password
+            <div class="title">
+                <i class="fa fa-key"></i> Forget Password
+            </div>
         </div>
     </div>
 </div>
