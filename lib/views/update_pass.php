d7370e8c3d09728a9087809c7695a1bd1a7862ca
JehanKandy <jehankandy@gmail.com>
2023-01-22 18:13:55 +0530

    Update update_pass.php

--- lib/views/update_pass.php
+++ lib/views/update_pass.php
@@ -3,5 +3,16 @@
 <?php include("../layouts/login_nav.php"); ?>
 <?php include("../function/function.php"); ?>
 
+<div class="update-pass">
+    <div class="container">
+        <div class="body">
+            <div class="title">
+                <i class="fas fa-key"></i>Update Password
+            </div>
+            
+        </div>
+    </div>
+</div>
+
 <?php include("../layouts/page_footer.php"); ?>
 <script src="../../js/script.js"></script>
