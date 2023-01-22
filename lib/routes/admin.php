00e9f2949694c01f4fbb9e68f3efcbeaf743e687
JehanKandy <jehankandy@gmail.com>
2023-01-22 21:06:47 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -2,6 +2,8 @@
 <?php include("../function/function.php"); ?>
 <?php include("../layouts/header.php"); ?>
 
-
+<div class="admin-content">
+    <a href="add_product.php"><button class="btn btn-primary">Add Product</button></a>
+</div>
 
 <script src="../../js/script.js"></script>
