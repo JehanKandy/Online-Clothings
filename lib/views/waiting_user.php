e4dd3a976ee90517f4e39dcc4a8d6b42f7253730
JehanKandy <jehankandy@gmail.com>
2023-01-22 06:28:51 +0530

    Add files via upload

---
+++ lib/views/waiting_user.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="../../css/style.css">
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/login_nav.php"); ?>
+<?php include("../function/function.php"); ?>
+
+<div class="waiting-content">
+    <div class="container">
+
+    </div>
+</div>
+
+<?php include("../layouts/page_footer.php"); ?>
+<script src="../../js/script.js"></script>
