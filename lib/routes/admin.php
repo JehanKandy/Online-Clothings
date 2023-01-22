e16f23fd8bd1ca4199d04c51d548611968cf3935
JehanKandy <jehankandy@gmail.com>
2023-01-22 21:04:51 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -1,3 +1,6 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
+<?php include("../function/function.php"); ?>
+<?php include("../layouts/header.php"); ?>
+
 
 <script src="../../js/script.js"></script>
