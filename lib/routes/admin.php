30b776c2bd6e2880e5bd3d922582743e44ccd25e
JehanKandy <jehankandy@gmail.com>
2023-01-26 08:39:41 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -2,8 +2,19 @@
 <?php include("../function/function.php"); ?>
 <?php include("../layouts/header.php"); ?>
 
+<?php 
+
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/logout.php");
+    }
+    admin_access();
+
+?>
+
 <div class="admin-content">
-    <a href="add_product.php"><button class="btn btn-primary">Add Product</button></a>
+    <div class="container">
+        <a href="add_product.php"><button class="btn btn-primary">Add Product</button></a>
+    </div>
 </div>
 
 <script src="../../js/script.js"></script>
