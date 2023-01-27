5f3d5c5f77725fc2583095cf34bd4a73a9e5208a
JehanKandy <jehankandy@gmail.com>
2023-01-27 12:10:37 +0530

    Update product_nav.php

--- lib/layouts/product_nav.php
+++ lib/layouts/product_nav.php
@@ -7,7 +7,7 @@
                 Pearl Clothings - All Products
             </div>
             <div class="right-side">
-                <a href="../../index.php"><button class="login-nav-btn">Back</button></a>
+                <a href="../../index.php"><button class="product-nav-btn">Back</button></a>
             </div>                       
         </div>
     </div>
