0fe8c0592c6b88ce323546c912bfbac532612699
JehanKandy <jehankandy@gmail.com>
2023-01-27 12:42:59 +0530

    Update all_products.php

--- lib/routes/all_products.php
+++ lib/routes/all_products.php
@@ -2,5 +2,15 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/product_nav.php"); ?>
 
+<div class="all-product-contant">
+    <div class="container">
+        <div class="title">All Prodcuts</div>
+        <div class="body">
+            hi all
+        </div>
+    </div>
+</div>
+
+
 <?php include("../layouts/page_footer.php"); ?>
 <script src="../../js/script.js"></script>
