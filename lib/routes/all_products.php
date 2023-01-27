f72d776d099265a648ddf1ae48d57f31824dfbe5
JehanKandy <jehankandy@gmail.com>
2023-01-27 11:54:27 +0530

    Update all_products.php

--- lib/routes/all_products.php
+++ lib/routes/all_products.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
 <?php include("../layouts/header.php"); ?>
-
+<?php include("../layouts/product_nav.php"); ?>
 
 <?php include("../layouts/page_footer.php"); ?>
 <script src="../../js/script.js"></script>
