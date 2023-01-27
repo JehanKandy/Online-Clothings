c10267f1a537c678ad240a2fa6405352075d2b8f
JehanKandy <jehankandy@gmail.com>
2023-01-27 11:55:17 +0530

    Add files via upload

---
+++ lib/layouts/product_nav.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="../../css/style.css">
+<link rel="stylesheet" href="../../css/dashboard.css">
+<nav>
+    <div class="product-nav-content">
+        <div class="container">
+            <div class="left-side">
+                Pearl Clothings - All Products
+            </div>
+            <div class="right-side">
+                <a href="../../index.php"><button class="login-nav-btn">Back</button></a>
+            </div>                       
+        </div>
+    </div>
+</nav>
+
+<script src="../../js/script.js"></script>
