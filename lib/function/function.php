433cbab1f96d855709a887dd4f3fab36af81dcd2
JehanKandy <jehankandy@gmail.com>
2023-01-29 06:32:41 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -556,7 +556,8 @@
                     <td>Date Of Birth : </td>
                     <td><input type='text' class='input-feild' value='".$user_row['dob']."' disabled></td>
                 </tr>
-                <tr>";
+                <tr>
+                    <td>User Type : </td>";
 
                 if($user_row['user_type'] == "admin"){
                     $user_data .= "<td><h1>Admin</h1></td>";              
