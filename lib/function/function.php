7131a315f3ddc03e7646ddc400ae86d35afcad2b
JehanKandy <jehankandy@gmail.com>
2023-01-29 06:52:39 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -568,10 +568,10 @@
                     <td style='padding-top:20px;'>Pending Status : </td>";
 
                 if($user_row['is_pending'] == 1){
-                    $user_data .="<td style='padding-top:20px;'><span class='user_pending_b'>Pending</span></td>";
+                    $user_data .="<td style='padding-top:20px;'><span class='user_pending_b'>Pending User</span></td>";
                 }
                 elseif($user_row['is_pending'] == 0){
-                    $user_data .="<td style='padding-top:20px;'><span class='user_not_pending_b'>Active</span></td>";
+                    $user_data .="<td style='padding-top:20px;'><span class='user_not_pending_b'>Active User</span></td>";
                 }
 
 
