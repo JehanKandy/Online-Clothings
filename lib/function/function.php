54b961f30835e4fa70525f03a569e2054fece119
JehanKandy <jehankandy@gmail.com>
2023-01-30 11:33:09 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -726,6 +726,8 @@
             if($username != $alredy_row['username']){
                 if($nic == $select_data_row['nic_no']){
                     $update_data = "UPDATE user_tbl SET username = '$username', fname = '$fn', lname = '$ln', address_user = '$address_user', gender = '$gender', dob = '$dob' WHERE nic_no = '$nic'";
+                    $update_data_result = mysqli_query($con, $update_data);
+                
                 }elseif($nic != $select_data_row['nic_no']){
                     return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                             <strong>Process Error</strong>Can not Process the Request..!
