f99da3c496018a3b9cde987365d163c5f8c3a6ee
JehanKandy <jehankandy@gmail.com>
2023-01-30 08:06:06 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -684,7 +684,18 @@
                         $user_data .="<td style='padding-top:20px;'><span class='user_not_pending_b'>Active User</span></td>";
                     }    
 
-                    
+                $user_data .="
+                    </tr>
+                    <tr>
+                        <td style='padding-top:20px;'>Active Status : </td>";
+                        
+                    if($user_row['is_active'] == 1){
+                        $user_data .="<td style='padding-top:20px;'><span class='user_active_b'>Active User</span></td>";
+                    }
+                    elseif($user_row['is_active'] == 0){
+                        $user_data .="<td style='padding-top:20px;'><span class='user_deactive_b'>Deactive User</span></td>";
+                    }
+
         echo $user_data;
     
     }
