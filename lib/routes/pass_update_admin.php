c623f46efafc0ceb0d0e43ae82bcdcb53dd76b64
JehanKandy <jehankandy@gmail.com>
2023-02-10 06:49:43 +0530

    Add files via upload

---
+++ lib/routes/pass_update_admin.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include("../function/function.php"); ?>
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/loged_nav.php"); ?>
+
+
+<script src="../../js/script.js"></script>
