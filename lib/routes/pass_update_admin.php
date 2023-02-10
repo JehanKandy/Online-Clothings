e73ae547442dba6c8605be638d026643dc227e87
JehanKandy <jehankandy@gmail.com>
2023-02-10 07:35:51 +0530

    Update pass_update_admin.php

--- lib/routes/pass_update_admin.php
+++ lib/routes/pass_update_admin.php
@@ -38,7 +38,8 @@
 		<hr>
         <?php 
             if(isset($_POST['update_dpass'])){
-                update_pass_data($_POST['old_pass'], $_POST['new_pass'], $_POST['new_cpass'])
+                update_pass_data($_POST['old_pass'], $_POST['new_pass'], $_POST['new_cpass']);
+                
             }
         
         ?>
