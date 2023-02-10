778183385ae27e963b9e01a94b180063d0c4c072
JehanKandy <jehankandy@gmail.com>
2023-02-10 07:59:41 +0530

    Add files via upload

---
+++ lib/routes/email_update_admin.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<?php include("../function/function.php"); ?>
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/loged_nav.php"); ?>
+
+<?php 
+
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/logout.php");
+    }
+    admin_access();
+
+?>
+
+<div class="app">
+	<div class="menu-toggle">
+		<div class="hamburger">
+			<span></span>
+		</div>
+	</div>
+
+	<aside class="sidebar">
+		<nav class="menu">
+			<?php //profile_img();?>
+			<p class="profile-name"><?php //user_id_loged();?></p>
+			<a href="admin.php" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+			<a href="members.php" class="menu-item"><i class="fas fa-user-alt"></i>Members 
+			<a href="admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin   
+			<a href="products.php" class="menu-item"><i class="fas fa-gifts"></i>Prodcuts  
+            <a href="plans.php" class="menu-item"><i class="fas fa-book-reader"></i>Plans  
+			<a href="my_account_admin.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
+		</nav>
+		
+	</aside>
+
+	<main class="content">
+		<h1>Update Password, <?php ?> </h1>
+		<hr>
+        <?php 
+            if(isset($_POST['update_dpass'])){
+                $result = update_pass_data(md5($_POST['old_pass']), md5($_POST['new_pass']), md5($_POST['new_cpass']));
+                echo $result;
+            }
+        
+        ?>
+
+		<?php update_pass_login(); ?>
+	</main>
+</div>
+
+<script src="../../js/script.js"></script>
