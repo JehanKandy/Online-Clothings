8004dcac4b1aa3cd1e90121f3762634070ec1256
JehanKandy <jehankandy@gmail.com>
2023-02-12 14:40:24 +0530

    Update update_pimg.php

--- lib/routes/update_pimg.php
+++ lib/routes/update_pimg.php
@@ -1,42 +1,23 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
-<?php include("../function/function.php"); ?>
-<?php include("../layouts/header.php"); ?>
-<?php include("../layouts/loged_nav.php"); ?>
+<?php include "../layouts/header.php";?>
+<?php include "../layouts/nav_loged_user.php";?>
 
 <?php 
-
     if(empty($_SESSION['LoginSession'])){
-        header("location:../views/logout.php");
+        header("location:../views/login.php");
     }
-    admin_access();
-
 ?>
 
-<div class="app">
-	<div class="menu-toggle">
-		<div class="hamburger">
-			<span></span>
-		</div>
-	</div>
+<?php 
+    if(isset($_POST['profile_img_update'])){
+        $result = update_profile_image($_FILES['images']['name']);
+        echo $result;
+    }
+?>
 
-	<aside class="sidebar">
-		<nav class="menu">
-			<?php profile_img();?>
-			<p class="profile-name"><?php user_id_loged();?></p>
-			<a href="admin.php" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
-			<a href="members.php" class="menu-item"><i class="fas fa-user-alt"></i>Members 
-			<a href="admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin   
-			<a href="products.php" class="menu-item"><i class="fas fa-gifts"></i>Prodcuts  
-			<a href="my_account_admin.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
-		</nav>
-		
-	</aside>
 
-	<main class="content">
-		<h1>Admin, <?php ?> </h1>
-		<hr>
-		<?php user_data(); ?>
-	</main>
+<div class="container">
+    <div class="update-user-profile-img">
+        <?php update_profile_img(); ?>
+    </div>
 </div>
-
-<script src="../../js/script.js"></script>
