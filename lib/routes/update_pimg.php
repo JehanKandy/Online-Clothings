0fb4a8db46c8cd43e7633079aa523663b2e963dc
JehanKandy <jehankandy@gmail.com>
2023-02-12 14:41:05 +0530

    Update update_pimg.php

--- lib/routes/update_pimg.php
+++ lib/routes/update_pimg.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
-<?php include "../layouts/header.php";?>
-<?php include "../layouts/nav_loged_user.php";?>
+<?php include("../function/function.php"); ?>
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/loged_nav.php"); ?>
 
 <?php 
     if(empty($_SESSION['LoginSession'])){
