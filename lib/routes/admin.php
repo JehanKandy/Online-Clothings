14cf180ea74962ca19c483130d1880ef8fb5e2e3
JehanKandy <jehankandy@gmail.com>
2023-02-12 14:09:07 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -27,8 +27,7 @@
 			<a href="members.php" class="menu-item"><i class="fas fa-user-alt"></i>Members &nbsp; <span class="count"><?php //count_members(); ?></span></a>
 			<a href="admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Admin  &nbsp; <span class="count"><?php //count_admins(); ?></span></a>
 			<a href="products.php" class="menu-item"><i class="fas fa-gifts"></i>Prodcuts  &nbsp; <span class="count"><?php //count_products(); ?></span></a>
-            <a href="plans.php" class="menu-item"><i class="fas fa-book-reader"></i>Plans  &nbsp; <span class="count"><?php //count_plans(); ?></span></a>
-			<a href="my_account_admin.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
+   			<a href="my_account_admin.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
 		</nav>
 		
 	</aside>
@@ -67,7 +66,7 @@
 				</div>
 				<div class="admin-item4">
 					<div class="admin-title">
-						<i class="fas fa-book-reader"></i> &nbsp;  Plans<br>
+						<i class="fas fa-money-bill-wave"></i> &nbsp;  Bills<br>
 					</div>
 					<hr class="qui-hr">
 					<div class="admin-body">
